080191d6467d7a8358bd2eff8905c642220ef505
Westley Feller <feller.westley@gmail.com>
2020-09-06 03:56:15 -0700

    Expensive seq complete

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,14 +1,31 @@
-# Your code here
+calculated = {}
 
 
 def expensive_seq(x, y, z):
-    # Your code here
+    if f"{x} {y} {z}" in calculated:
+        return calculated[f"{x} {y} {z}"]
 
+    if x <= 0:
+        calculated[f"{x} {y} {z}"] = y + z
+        return y + z
+
+    if x > 0:
+        calculated[f"{x} {y} {z}"] = (
+            expensive_seq(x - 1, y + 1, z)
+            + expensive_seq(x - 2, y + 2, z * 2)
+            + expensive_seq(x - 3, y + 3, z * 3)
+        )
+
+        return (
+            expensive_seq(x - 1, y + 1, z)
+            + expensive_seq(x - 2, y + 2, z * 2)
+            + expensive_seq(x - 3, y + 3, z * 3)
+        )
 
 
 if __name__ == "__main__":
     for i in range(10):
-        x = expensive_seq(i*2, i*3, i*4)
+        x = expensive_seq(i * 2, i * 3, i * 4)
         print(f"{i*2} {i*3} {i*4} = {x}")
 
     print(expensive_seq(150, 400, 800))
