6463d47b2f9a71d2c85c25994c090e7e0a59bc75
Westley Feller <feller.westley@gmail.com>
2020-09-06 03:38:05 -0700

    Lookup complete

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,21 +1,34 @@
-# Your code here
+import math
+import random
+
+
+calculated = {}
 
 
 def slowfun_too_slow(x, y):
     v = math.pow(x, y)
     v = math.factorial(v)
-    v //= (x + y)
+    v //= x + y
     v %= 982451653
 
     return v
+
 
 def slowfun(x, y):
     """
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
-    # Your code here
+    if f"{x} {y}" in calculated:
+        return calculated[f"{x} {y}"]
 
+    v = math.pow(x, y)
+    v = math.factorial(v)
+    v //= x + y
+    v %= 982451653
+
+    calculated[f"{x} {y}"] = v
+    return v
 
 
 # Do not modify below this line!
@@ -23,4 +36,4 @@ def slowfun(x, y):
 for i in range(50000):
     x = random.randrange(2, 14)
     y = random.randrange(3, 6)
-    print(f'{i}: {x},{y}: {slowfun(x, y)}')
+    print(f"{i}: {x},{y}: {slowfun(x, y)}")
